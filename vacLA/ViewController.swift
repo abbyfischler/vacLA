274044ce146fc0b043cbd624eafd5fe0d68617b4
Abby Fischler <abigail.fischler@gmail.com>
2021-04-05 10:41:58 -0700

    Added Hebrew and Reviews Link

--- vacLA/ViewController.swift
+++ vacLA/ViewController.swift
@@ -18,8 +18,9 @@ class ViewController: UIViewController, WKUIDelegate{
     @IBAction func openMap(_ sender: Any) {
         UIApplication.shared.open(URL(string:"https://www.google.com/maps/d/u/0/edit?mid=16LrbL9tMzlxDzG1FEdSaP8nBntAPd2j6&usp=sharing")!)
     }
-    @IBAction func reviews(_ sender: Any) {
-        UIApplication.shared.open(URL(string:"https://www.google.com/maps/d/u/0/edit?mid=16LrbL9tMzlxDzG1FEdSaP8nBntAPd2j6&usp=sharing")!)
-    }
+    
   
+    @IBAction func reviews(_ sender: Any) {
+        UIApplication.shared.open(URL(string:"https://padlet.com/mnadel96/fp5grh3piqm3tckz")!)
+    }
 }
